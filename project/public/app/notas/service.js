d67513e7f515f84bd91a16155f6079b43c0a09ba
anderleand <andersonleandro.desouza@gmail.com>
2021-05-08 20:27:43 -0300

    Update service.js
    Compondo funções

--- project/public/app/notas/service.js
+++ project/public/app/notas/service.js
@@ -1,9 +1,9 @@
 import { handleStatus } from '../utils/promise-helpers.js'
+import { partialize } from '../utils/operators.js'
 
-const sumItems = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, item) => item.filter(item => item.codigo == code);
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 const API = 'http://localhost:3000/notas';
 
@@ -11,10 +11,23 @@ export const notasService = {
 
     listAll() {
 
-        return fetch(API).then(handleStatus);
+        return fetch(API)
+            .then(handleStatus)
+            .catch(err => {
+                console.log(err);
+                return Promise.reject('Não foi possível obter as notas fiscais.');
+            })
     },
 
     sumItems(code) {
-        return this.listAll().then(sumItems(code));
+
+        const filterItems = partialize(filterItemsByCode, code);
+        return this.listAll().then(notas =>
+            sumItemsValue(
+                filterItems(
+                    getItemsFromNotas(notas)
+                )
+            )
+        );
     }
 }
