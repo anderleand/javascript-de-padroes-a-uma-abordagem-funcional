ecaf15c2435d88d778225b0e0b916a0561daaf48
anderleand <andersonleandro.desouza@gmail.com>
2021-05-10 17:06:04 -0300

    Update app.js
    Limitando operações dentro de um determinado período - debounceTime

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,14 +1,16 @@
 import { log } from './utils/promise-helpers.js'
 import './utils/array-helpers.js'
 import { notasService as service } from './notas/service.js'
-import { takeUntil } from './utils/operators.js'
+import { takeUntil, debounceTime } from './utils/operators.js'
 
-const operation = takeUntil(3, () =>
-    service
-        .sumItems('2143')
-        .then(log)
-        .catch(log)
+const action = debounceTime(500,
+    takeUntil(3, () =>
+        service
+            .sumItems('2143')
+            .then(log)
+            .catch(log)
+    )
 );
 
 
-document.querySelector('#myButton').onclick = () => operation();
+document.querySelector('#myButton').onclick = () => action();
